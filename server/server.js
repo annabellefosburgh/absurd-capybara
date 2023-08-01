62a5eb3c93901706c4120649911148d241d21717
annabellefosburgh <afosburgh20@gmail.com>
2023-07-31 19:27:53 -0500

    Adding comments to map out what I need to change in the server.js file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
+//Adding ApolloServer, typeDefs, resolvers, middleware
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//New Apollo Server initiation
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +18,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//Start Apollo Server using GraphQL Schema
+
+//app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+//Start server
