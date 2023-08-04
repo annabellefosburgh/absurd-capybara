acca1641f687808264f73429ad8685554a5c22e2
annabellefosburgh <afosburgh20@gmail.com>
2023-08-03 20:13:12 -0500

    Adding apollo client and graphql to app.js, creating new apollo client to server.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 //Import apollo client and provider
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider, 
+  createHttpLink,
+} from "@apollo/client";
+
+//creating graphql link, authlink
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 //New Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-//const routes = require('./routes');
+const routes = require('./routes');
 //Adding ApolloServer, typeDefs, resolvers, middleware
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 //New Apollo Server initiation
+const server = new ApolloServer({
+  typeDefs,
+  resolvers, 
+  context: authMiddleware,
+  persistedQueries: false, 
+});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,11 +29,24 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 //Start Apollo Server using GraphQL Schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
-//app.use(routes);
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`Server now running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+    });
+  });
+};
+
+
+app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
 
 //Start server
+startApolloServer(typeDefs, resolvers);
